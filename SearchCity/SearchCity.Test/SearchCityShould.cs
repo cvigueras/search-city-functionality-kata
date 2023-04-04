051f6a31157b1a084b46f55dbb5deefdd916ccdc
cvigueras <carlosj.vigueras@gmail.com>
2023-04-04 09:51:05 +0200

    Chore: Init project.

---
+++ SearchCity/SearchCity.Test/SearchCityShould.cs
@@ -0,0 +1,16 @@
+namespace SearchCity.Test
+{
+    public class SearchCityShould
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
